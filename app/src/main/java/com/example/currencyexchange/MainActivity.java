b1430f6659d278750ccde69b7813d5aee0b8b873
Annikas Mac <ammfsee@gmail.com>
2024-03-13 01:35:43 +0100

    Käse-Brot
    add content on info page and some small changes

--- app/src/main/java/com/example/currencyexchange/MainActivity.java
+++ app/src/main/java/com/example/currencyexchange/MainActivity.java
@@ -31,6 +31,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.OutputStreamWriter;
